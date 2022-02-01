10c6a3fbbfaab31b2a764e4c6396adf0123b658a
James Cook <james@jaminit.co.uk>
2022-02-01 19:56:10 +0000

    feat: show more button

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -1,53 +1,65 @@
 import {
+  Grid,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  Button,
+  Box,
 } from "@mui/material";
 import { NextPage } from "next";
 import { Game } from "../lib/types";
 import useSWR, { Fetcher } from "swr";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 interface Props {
   game: Game;
-  length?: number;
 }
 const fetcher: Fetcher<Array<any>> = (url: string) =>
   fetch(url).then((res) => res.json());
 
-const Leaderboard: NextPage<Props> = ({ game, length = 3 }) => {
+const Leaderboard: NextPage<Props> = ({ game }) => {
+  const [cutOffPoint, setCutOffPoint] = useState(3);
+  function toggleCutOff(game: Game) {
+    setCutOffPoint(cutOffPoint == 3 ? 10 : 3);
+  }
   const { data, error } = useSWR(
     "https://api.playhive.com/v0/game/monthly/" + game.toLowerCase(),
     fetcher
   );
-  const leaderboardData = data?.slice(0, length);
-
+  const leaderboardData = data?.slice(0, cutOffPoint);
   return (
-    <TableContainer sx={{ my: 5 }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Position</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell>Games Played</TableCell>
-            <TableCell>Games Won</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {leaderboardData &&
-            leaderboardData.map((row) => (
-              <TableRow key={row.index}>
-                <TableCell>{row.human_index ?? "N/A"}</TableCell>
-                <TableCell>{row.username ?? "N/A"}</TableCell>
-                <TableCell>{row.victories ?? "N/A"}</TableCell>
-                <TableCell>{row.played ?? "N/A"}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box>
+      <TableContainer sx={{ my: 5, mb: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Position</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Games Played</TableCell>
+              <TableCell>Games Won</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {leaderboardData &&
+              leaderboardData.map((row) => (
+                <TableRow key={row.index}>
+                  <TableCell>{row.human_index ?? "N/A"}</TableCell>
+                  <TableCell>{row.username ?? "N/A"}</TableCell>
+                  <TableCell>{row.victories ?? "N/A"}</TableCell>
+                  <TableCell>{row.played ?? "N/A"}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Grid container justifyContent="center">
+        <Button onClick={() => toggleCutOff(game)}>
+          Show {cutOffPoint != 3 ? "Less" : "More"}
+        </Button>
+      </Grid>
+    </Box>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import { Box, Container, Typography, Grid } from "@mui/material";
+import { Container, Typography, Grid, Button } from "@mui/material";
 import type { NextPage } from "next";
 import Leaderboard from "../components/Leaderboard";
 import { GAMES, GAME_FRIENDLY_NAME } from "../lib/consts";
+import { useState } from "react";
+import { Game } from "../lib/types";
 
 const Home: NextPage = () => {
   return (
-    <Container sx={{ mt: 5 }}>
+    <Container sx={{ mt: 5, mb: 10 }}>
       <Typography variant="h3">Hive Leaderboards</Typography>
-      <Grid container spacing={10}>
+      <Grid container spacing={10} sx={{ pt: 3 }}>
         {GAMES.map((game) => (
           <Grid item key={game}>
             <Typography variant="h4">{GAME_FRIENDLY_NAME[game]}</Typography>
