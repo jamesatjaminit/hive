006a6fe1d133160a6c9e06e5e9f0533a2f72a020
James Cook <james@jaminit.co.uk>
2022-03-20 22:37:36 +0000

    feat: use mantine form hook

--- pages/leaderboards-monthly.tsx
+++ pages/leaderboards-monthly.tsx
@@ -1,23 +1,40 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Leaderboard from "../components/Leaderboard";
 import { GAMES, GAME_FRIENDLY_NAME } from "../lib/consts";
-import { useState, FormEvent } from "react";
+import { useState } from "react";
 import { NextSeo } from "next-seo";
 import { Box, Button, Grid, TextInput, Container } from "@mantine/core";
 import { User } from "tabler-icons-react";
+import { useForm } from "@mantine/form";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+interface Props {
+  username: string | undefined;
+}
+const LeaderboardsMonthly: NextPage<Props> = ({ username }) => {
+  const router = useRouter();
+  // @ts-expect-error
+  const usernameFromParams: string | undefined =
+    username ?? router.query.username;
+  const [lookupUsername, setLookupUsername] = useState<string | undefined>(
+    usernameFromParams
+  );
+  const usernameForm = useForm({
+    initialValues: {
+      username: "",
+    },
+    validate: {
+      username: (value) => (value ? null : "Enter a username"),
+    },
+  });
+  useEffect(() => {
+    usernameFromParams &&
+      usernameForm.setFieldValue("username", usernameFromParams);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [usernameFromParams]);
 
-const LeaderboardsMonthly: NextPage = () => {
-  const [lookupUsername, setLookupUsername] = useState<string>();
-  const [usernameFieldError, setUsernameFieldError] = useState<string | null>();
-  function submitUsername(e: FormEvent) {
-    e.preventDefault();
-    // @ts-expect-error
-    const username = e.target?.username_input?.value;
-    if (username) {
-      setLookupUsername(username);
-    } else {
-      setUsernameFieldError("You must enter a username");
-    }
+  function handleUsernameFormSubmit(values: typeof usernameForm.values) {
+    setLookupUsername(values.username);
   }
   return (
     <>
@@ -37,16 +54,12 @@ const LeaderboardsMonthly: NextPage = () => {
       <Container size="xl">
         <h1>Hive Monthly Leaderboards</h1>
         <Box sx={{ maxWidth: 250 }}>
-          <form onSubmit={submitUsername}>
+          <form onSubmit={usernameForm.onSubmit(handleUsernameFormSubmit)}>
             <TextInput
-              variant="filled"
               label="Username"
-              id="username_input"
-              size="sm"
               type="text"
-              error={usernameFieldError}
-              onChange={() => setUsernameFieldError(null)}
               icon={<User />}
+              {...usernameForm.getInputProps("username")}
             />
             <Button variant="filled" type="submit" sx={{ marginTop: 10 }}>
               Search
@@ -65,5 +78,9 @@ const LeaderboardsMonthly: NextPage = () => {
     </>
   );
 };
-
 export default LeaderboardsMonthly;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: { username: context.query.username ?? null },
+  };
+};
